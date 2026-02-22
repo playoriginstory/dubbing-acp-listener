13ec51174694226b488cfac297b68dde367b3bbc
sean <playoriginstory@gmail.com>
2026-02-22 21:23:53 +0100

    fix

--- index.js
+++ index.js
@@ -40,15 +40,14 @@ const s3 = new S3Client({
 async function uploadToS3(buffer, key, contentType) {
   await s3.send(
     new PutObjectCommand({
-      Bucket: process.env.S3_BUCKET_NAME,
-      Key: key,
-      Body: buffer,
-      ContentType: contentType,
+    Bucket: process.env.AWS_S3_BUCKET,      
+    Key: key,
+    Body: buffer,
+    ContentType: contentType,
     })
   );
 
-  return `https://${process.env.S3_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${key}`;
-}
+  return `https://${process.env.AWS_S3_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com/${key}`;}
 
 async function safeDeliver(job, status, dubbedFileUrl = "") {
   try {
